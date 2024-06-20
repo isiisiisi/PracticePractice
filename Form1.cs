01117171ef093583b4f3108054e07ae6c425424b
jpelec3a10 <jpelec3a10@BCL510>
2024-06-20 10:33:40 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+namespace PracticePractice
+{
+    public partial class calculator : Form
+    {
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_MouseClick(object sender, MouseEventArgs e)
+        {
+            //lblTest.Text = txtTest.Text;
+            //lblTest.Text = txtTest.Text + txtPractice2.Text;
+        }
+
+        private void txtPractice2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
